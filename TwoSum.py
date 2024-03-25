b65251871ad7106d9ac9ba771dffafef7a395b86
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2024-03-25 13:46:53 -0400

    first commit

---
+++ TwoSum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_map = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
+    sol = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = sol.twoSum(nums, target)
+    print(result)       
